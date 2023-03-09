56abfdb1ae5ce7234c785ea2174874816967d7f2
Abhinna Chandra Biswal <50557907+abhinna-biswal@users.noreply.github.com>
2023-03-09 06:26:59 +0000

    Updated home.html and app.py

--- app.py
+++ app.py
@@ -2,10 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'Location': 'Bengaluru, India',
+  'salary': 'Rs. 10,00,000',
+}, {
+  'id': 2,
+  'title': 'Data Scientist',
+  'Location': 'Delhi, India',
+  'salary': 'Rs. 15,00,000',
+}, {
+  'id': 3,
+  'title': 'Frontend Engineer',
+  'Location': 'Remote',
+}, {
+  'id': 3,
+  'title': 'Backend Engineer',
+  'Location': 'San Fransisco, USA',
+  'salary': '$120, 000',
+}]
+
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name='Abhinna')
 
 
 if __name__ == '__main__':
